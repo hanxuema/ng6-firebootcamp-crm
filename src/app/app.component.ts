40b5a59b50a471131a176a41d6911db7f136de39
Xavier Xie <xieyuehao@gmail.com>
2018-10-22 10:52:03 +1000

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  //
   title = 'firebootcamp-crm';
+
+  //event
+  keyPressed(event) {
+    this.title = event.target.value;
+
+  }
 }
+
+//selector is the name of the component
+
